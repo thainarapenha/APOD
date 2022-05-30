85553b3831d4f0088218a97fe1c4e4fb1233b382
Thainara <thainarapenha@alu.ufc.br>
2022-05-29 21:29:43 -0300

    nova versão de codigo para solicitacao da API

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
+let btn = document.getElementById("#btnEnviar");
+// let data = document.getElementById("#IDdata");
+let img = document.getElementById("#areaIMG");
 
-// função do botão enviar
-$("#btnEnviar").click(function(){
-    chamarAPI();
-});
-// a ação do botão retorna a chamada de outra função
-function chamarAPI(){
-    const data = $("#data").val();
+function chamando(){
+    let data = document.getElementById("#IDdata").val()  
+    clique(data)
 }
 
-function pedidoAPI(procurar){
-    let url = "https://api.nasa.gov/planetary/apod?api_key=ZxgxSULl0vkWFRQ6MyHrY3tSKB449bO3kO0wQkN5"
+// função que vai consumir a API quando for chamada no clique do botão
+function clique(){
+    const pedirAPI = (url) => {// recebe a url que será passada mais a frente
+        const pedindo = (resolve, reject) => {
+            // função responsável por solicitar API, recebendo caminho pela url
+            fetch(url)
+            .then(( response ) => {
+                // verifica qual erro aconteceu e o status da solicitação
+                if(!response.ok) throw Error('erro ao executar' + response.status);
+                return response.json();
+            })
+            .then(resolve)
+            .catch (reject)
+        }
+        return new Promise(pedindo);
+    }
+    // passa o caminho da url para consumir a API
+    pedirAPI('https://api.nasa.gov/planetary/apod?api_key=ZxgxSULl0vkWFRQ6MyHrY3tSKB449bO3kO0wQkN5').then(console.log).catch(console.error);
+}
 
-    fetch(`${url}&date=${procurar}`)
+// $(document).on('click', '#btnEnviar', function () {
+//     let data = $("#data").val();    
+//     ChamadaAPI(data);
+// });
+
+function ProcHTML(response){
+    let img = document.querySelector("areaIMG")
+    let txt = document.querySelector("areaTXT")
+    img.innerHTML = `
+    <img src=" + response.url + ">`
+    txt.innerHTML = `
+    <p> + response.explanation + </p>');`
 }
