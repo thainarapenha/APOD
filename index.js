fdad7c64e571e4c356b7cf6f2ead77e9cef0167f
Thainara <thainarapenha@alu.ufc.br>
2022-05-30 13:42:42 -0300

    adicionando códig js funcionando

--- index.js
+++ index.js
@@ -1,42 +1,51 @@
-let btn = document.getElementById("#btnEnviar");
-// let data = document.getElementById("#IDdata");
-let img = document.getElementById("#areaIMG");
+let btn;
+let date; 
+let img;
 
-function chamando(){
-    let data = document.getElementById("#IDdata").val()  
-    clique(data)
+console.log(document.getElementById("IDdata").value);
+
+function ChamadaAPI(){
+    clique();
 }
 
 // função que vai consumir a API quando for chamada no clique do botão
-function clique(){
+function clique(){    
     const pedirAPI = (url) => {// recebe a url que será passada mais a frente
         const pedindo = (resolve, reject) => {
             // função responsável por solicitar API, recebendo caminho pela url
             fetch(url)
-            .then(( response ) => {
+            .then(( resp ) => {
                 // verifica qual erro aconteceu e o status da solicitação
-                if(!response.ok) throw Error('erro ao executar' + response.status);
-                return response.json();
+                if(!resp.ok) throw Error('erro ao executar' + resp.status);
+                return resp.json();
             })
-            .then(resolve)
+            // se tiver tudo certo, exibe o que foi encontrado
+            .then(function (resolve){
+                ProcHTML(resolve)
+            })
             .catch (reject)
         }
         return new Promise(pedindo);
     }
-    // passa o caminho da url para consumir a API
-    pedirAPI('https://api.nasa.gov/planetary/apod?api_key=ZxgxSULl0vkWFRQ6MyHrY3tSKB449bO3kO0wQkN5').then(console.log).catch(console.error);
+    // passa o caminho da url + data para consumir a API
+    pedirAPI('https://api.nasa.gov/planetary/apod?api_key=ZxgxSULl0vkWFRQ6MyHrY3tSKB449bO3kO0wQkN5&date=' + IDdata.value ).then(console.log).catch(console.error);
 }
 
-// $(document).on('click', '#btnEnviar', function () {
-//     let data = $("#data").val();    
-//     ChamadaAPI(data);
-// });
+$(document).on('click', '#IDEnviar', function () {    
+    btn = document.getElementById("IDEnviar");
+    data = document.getElementById("IDdata");
+    img = document.getElementById("IDimg");   
+    text = document.getElementById("IDtxt");
+    
+    ChamadaAPI();
+});
 
-function ProcHTML(response){
-    let img = document.querySelector("areaIMG")
-    let txt = document.querySelector("areaTXT")
-    img.innerHTML = `
-    <img src=" + response.url + ">`
-    txt.innerHTML = `
-    <p> + response.explanation + </p>');`
+function ProcHTML(response){ 
+    console.table(response);    
+    
+    var image = new Image();
+    image.src = response.url;
+    img.append(image);
+
+    text.append(response.explanation )
 }
